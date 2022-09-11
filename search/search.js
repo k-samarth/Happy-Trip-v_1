0d2e64c7219647a636feddc6ead614ca5b816b3a
samarth.narasimha <samarth.narasimha@skillassure.com>
2022-09-11 17:22:25 +0530

    airline dropdown completion

--- search/search.js
+++ search/search.js
@@ -10,34 +10,103 @@
 // 4. Solution -> a. class, b. function object, c. javascript object.
 // 5. Lovely, Beautiful and Professional UI.
 // 6. Agenda, Welcome, Problem, Solution, Key highlights, best practices.
-function Search()
+
+function fetchAirport()
 {
-    // 1. Validate data Entered in the form
-    // 2. Implement Rules in Seperate Functions
-    // 3. If Valid: Import Classes Needed
-    // 4. Create an Model for SearchInfo
-    // 5. Fill information for SearchInfo
-    // 6. Call the SearchManager -> Searchfunction
-    // 19. Upon success redirect to Search Results.
-    validateSearchInfo();
+    console.log("Fetch Airport Here");
+    fetch('../service/airports.json')
+            .then(function (response) {
+                // console.log(fetch('flight.json'));
+                return response.json();
+                
+            })
+            .then(function (data) {
+                for(let i in data.airports){
+                    var x = document.getElementById("fromCity");
+                    var option = document.createElement("option")
+                    option.text = data.airports[i].IATA_code+" : "+ data.airports[i].airport_name+", "+ data.airports[i].city_name;
+                    x.add(option);
+                }
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
 }
-function validateSearchInfo(searchInfo)
+fetchAirport()
+
+function fetchAirportTo()
 {
-    validateLocation();
+    // console.log("Fetch Airport Here");
+    fetch('../service/airports.json')
+            .then(function (response) {
+                // console.log(fetch('flight.json'));
+                return response.json();
+                
+            })
+            .then(function (data) {
+                for(let i in data.airports){
+                    var x = document.getElementById("toCity");
+                    var option = document.createElement("option")
+                    option.text = data.airports[i].IATA_code+" : "+ data.airports[i].airport_name+", "+ data.airports[i].city_name;
+                    x.add(option);
+                }
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+}
+fetchAirportTo()
+
+// Disable The Arrive On for Round Trip
+var arriveOn1 = document.querySelector(".arriveOn1")
+arriveOn1.addEventListener("click", disableArriveOn);
+function disableArriveOn()
+{
+    document.getElementById("disable").innerHTML="";
+}
+
+// Enable The Arrive On for One Way
+var arriveOn = document.querySelector(".arriveOn")
+arriveOn.addEventListener("click", enableArriveOn);
+function enableArriveOn()
+{
+    document.getElementById("disable").innerHTML=`<label for="exampleFormControlInput1" id="arriveOn">Arrive on </label>
+    <input type="date" class="form-control" id="dob">`;
+}
+// import {SearchInfo} from '../Models/' 
+// function Search()
+// {
+//     // 1. Validate data Entered in the form
+//     // 2. Implement Rules in Seperate Functions
+//     // 3. If Valid: Import Classes Needed
     
-    validateTravelDates();
+//     // 4. Create an Model for SearchInfo
+//     // 5. Fill information for SearchInfo
+//     // 6. Call the SearchManager -> Searchfunction
+//     // 19. Upon success redirect to Search Results.
+//     validateSearchInfo();
+// }
+// function validateSearchInfo(searchInfo)
+// {
+//     validateLocation();
     
-    validateNumberofTickets();
-}
-function validateLocation()
-{
+//     validateTravelDates();
+    
+//     validateNumberofTickets();
+// }
+// function validateLocation()
+// {
 
-}
-function validateTravelDates()
-{
+// }
+// function validateTravelDates()
+// {
 
-}
-function validateNumberofTickets()
-{
+// }
+// function validateNumberofTickets()
+// {
 
-}
+// }
+
+// Function to add Airport 
+
+

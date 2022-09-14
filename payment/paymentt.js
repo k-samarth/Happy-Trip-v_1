9a0a95a4aac02bd5ac279253fe64f3415bfbabfa
Shende <pshende@galaxe.com>
2022-09-14 12:54:52 +0530

    payment validations

--- payment/paymentt.js
+++ payment/paymentt.js
@@ -2,18 +2,11 @@ function display1() {
     var a= document.getElementById('cardOwner').value;
     var b = document.getElementById('cardNumber').value;
     var c = document.getElementById('month').value;
-    var d= document.getElementById('year').value;
+    // var d= document.getElementById('year').value;
     var e= document.getElementById('cvv').value;
     
     console.log(a);
 
-    // if(isNaN(a)){
-    //     alert("Enter correct card no.");
-    // }else if(isNaN(d)){
-    //     alert("Enter correct correct cvv");
-    // }else{
-    
-    // alert("payment cleared")}
     var regName = /^[a-zA-Z]+ [a-zA-Z]+/;
     
     if(!regName.test(a)){
@@ -25,27 +18,41 @@ function display1() {
     if (isNaN(b)){
         alert("Enter correct card no.")
     }else if(b.length!=16){
-        alert("  card length is wrong ")
+        alert(" Card number should be 16 digits only")
     }
 
-    if (isNaN(c)){
-        alert("Enter correct month.")
-    }else if(c.length!=2){
-        alert(" month length is wrong ")
-    }
+    // if (isNaN(c)){
+    //     alert("Enter correct month.")
+    // }else if(c.length!=2){
+    //     alert(" month length is wrong ")
+    // }
 
-    if (isNaN(d)){
-        alert("Enter correct year.")
-    }else if(d.length!=4){
-        alert(" Year length is wrong ")
-    }
+   
 
     if (isNaN(e)){
         alert("Enter correct cvv.")
     }else if(e.length!=3){
-        alert(" cvv length is wrong ")
+        alert(" CVV should be 3 digits only ")
     }
 
    
 
 }
+window.onload=setMinDate();
+function setMinDate() {
+
+    month.min =
+  
+      new Date().getFullYear() +
+  
+      "-" +
+  
+      +"0" +
+  
+      parseInt(new Date().getMonth() + 1) +
+  
+      "-" +
+  
+      new Date().getDate();
+  
+  }

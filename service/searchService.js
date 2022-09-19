99810acf52d6c874502a2c1f679d756f2cb0e6ed
samarth.narasimha <samarth.narasimha@skillassure.com>
2022-09-19 14:52:06 +0530

    Try to change fetch

--- service/searchService.js
+++ service/searchService.js
@@ -9,7 +9,7 @@ var cities = [];
 function fetchAirport() {
   console.log("Fetch Airport Here");
 
-  fetch("../service/airports.json")
+  fetch("https://github.com/k-samarth/Happy-Trip-v_1/blob/old-state/service/flightSchedules.json")
     .then(function (response) {
       // console.log(fetch('flight.json'));
       return response.json();
